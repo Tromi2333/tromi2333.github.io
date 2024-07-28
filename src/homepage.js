64c7560edcf4ae421b28c104ae84acc37ed4ed4b
Tromi2333 <152354110+Tromi2333@users.noreply.github.com>
2024-07-28 23:41:31 +0800

    Update homepage.js

--- src/homepage.js
+++ src/homepage.js
@@ -1,5 +1,18 @@
 var swplinkbutton = document.getElementById("swplinkbutton");
+var test = document.getElementById("test");
 swplinkbutton.onclick = function()
 {
     window.open("somewebsitespage/");
+}
+test.onclick = function()
+{
+    fetch(pic/icon.ico)
+      .then(response => response.blob())
+      .then(blob => {
+        const link = document.createElement('a');
+        link.href = URL.createObjectURL(blob);
+        link.download = icon.ico;
+        link.target = "_blank"; // 可选，如果希望在新窗口中下载文件，请取消注释此行
+        link.click();
+      });
 }
