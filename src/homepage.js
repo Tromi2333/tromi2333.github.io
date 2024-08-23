3dfd88263a8716dec9569e2d1a8b460c7932ec95
Tromi2333 <152354110+Tromi2333@users.noreply.github.com>
2024-08-24 00:07:00 +0800

    Add files via upload

--- src/homepage.js
+++ src/homepage.js
@@ -1,33 +1,10 @@
 var swplinkbutton = document.getElementById("swplinkbutton");
-var test = document.getElementById("test");
+var smdownloadpagebutton = document.getElementById("smdownloadpagebutton");
 swplinkbutton.onclick = function()
 {
     window.open("somewebsitespage/");
 }
-test.onclick = function download()
+smdownloadpagebutton.onclick = function()
 {
-    var path = "../pic/icon.ico";
-    var name = "icon.ico";
-    const xhr = new XMLHttpRequest();
-    xhr,open('get', path);
-    xhr.responseType = 'blob';
-    xhr.send();
-    xhr.onload = function()
-    {
-        if(this.status === 200 || this.status === 304)
-        {
-            const filereader = new FileReader();
-            filereader.readAsDataURL(this.response);
-            filereader.onload = function()
-            {
-                const a = document.createElement('a');
-                a.style.display = 'none';
-                a.href = this.result;
-                a.download = name;
-                document.body.appendChild(a);
-                a.click();
-                document.body.removeChild(a);
-            };
-        }
-    };
+    window.open("supermoddownloadpage/");
 }
