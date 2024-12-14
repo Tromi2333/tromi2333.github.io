a807704dc71a7edbea40e0c2b59cd162a661190a
Tromi2333 <152354110+Tromi2333@users.noreply.github.com>
2024-12-14 13:30:10 +0800

    Add files via upload

--- src/homepage.js
+++ src/homepage.js
@@ -1,10 +1,5 @@
 var swplinkbutton = document.getElementById("swplinkbutton");
-var smdownloadpagebutton = document.getElementById("smdownloadpagebutton");
 swplinkbutton.onclick = function()
 {
     window.open("somewebsitespage/");
-}
-smdownloadpagebutton.onclick = function()
-{
-    window.open("supermoddownloadpage/");
 }
